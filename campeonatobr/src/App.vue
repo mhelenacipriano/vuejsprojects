b25ad5bd440aeb52a37238b6e9e56acd79a82fd0
Maria Helena Cipriano <mariahelenatw@gmail.com>
2021-10-01 18:33:41 -0300

    primeira tela completa

--- campeonatobr/src/App.vue
+++ campeonatobr/src/App.vue
@@ -1,67 +1,37 @@
 <template>
   <v-app>
-  <div>
-     <v-app-bar
-      color="green darken-4"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <div>
+      <v-app-bar color="green darken-4" dark>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-    </v-app-bar>
+        <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+      </v-app-bar>
 
       <v-spacer></v-spacer>
-
-        <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-     
-  </div>
+    </div>
     <v-main>
       
+      <Inicio />
+      <AppTopBar />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Inicio from "./components/Inicio.vue";
+import AppTopBar from "./components/shared/AppTopBar.vue";
 
 export default {
-  name: 'App',
-  
+  name: "App",
 
   components: {
+    Inicio,
+    AppTopBar,
   },
 
   data: () => ({
     drawer: false,
-      group: null,
+    group: null,
   }),
 };
 </script>
---
+++ campeonatobr/src/components/shared/AppTopBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="AppTopBar">
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten-2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar",
+};
+</script>
+
+<style scoped>
+</style>
