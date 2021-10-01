309fc56d285f1c244db28ec6922ed1a58245477a
Maria Helena Cipriano <mariahelenatw@gmail.com>
2021-10-01 02:56:22 -0300

    contador atualizado

---
+++ contador/src/components/contaNum.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="contaNum">
+        <h1>Esse é um contadorzinho besta</h1>
+        <p>{{numero}}</p>
+        <button @click="contadorUp()" type="button">+</button>
+        <button @click="contadorDown()" type="button">-</button>
+
+    </div>    
+</template>
+
+<script>
+export default ({
+    name: 'contaNum',
+
+    data(){
+        return {
+            numero: 0
+        }
+    },
+
+    methods:{
+        contadorUp(){
+            this.numero++;
+            },
+
+        contadorDown(){
+            this.numero--;
+            }
+    }
+    
+})
+</script>
+
+<style scoped>
+
+</style>
